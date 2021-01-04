1999dc4803303766f8f8da40287ab97e55d0a0f0
iami5246 <iami5246@naver.com>
2021-01-04 15:50:36 +0900

    초기 커밋.

---
+++ HomePageWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using LGUPlus.Common;
+
+
+namespace HomePageWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 메인 페이지 호출 액션 메소드
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            //사용자 정보관리 서비스를 이용해 특정 사용자 정보를 조회해온다.
+            UserService service = new UserService();
+            var user = service.GetUserData("eddy");
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ LGUPlus.Common/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LGUPlus.Common
+{
+    /// <summary>
+    /// 사용자 정보 구조 정의 모델
+    /// </summary>
+    public class UserModel
+    {
+        public string UserName { get; set; }
+
+        public string UserEmail { get; set; }
+    }
+}
